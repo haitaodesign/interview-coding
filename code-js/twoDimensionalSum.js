4ef3a14247ad3ea1c31ab0c4ca6d8ad3c296c87b
Haitao Lee <lihaitaodesign@gmail.com>
2022-06-12 16:45:21 +0800

    feat: 完成面试题测试编写

---
+++ code-js/twoDimensionalSum.js
@@ -0,0 +1,21 @@
+/**
+ * 二维数组中，每个数字相加的和
+ * @param {Array} array 二维数组
+ * @returns number
+ */
+var twoDimensionalSum = function (array) {
+    var sum = 0
+    var oneArray = []
+    // 数组降维
+    array.map(function(item){
+        oneArray = oneArray.concat(item)
+    })
+    // 求和
+    sum = oneArray.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue
+    })
+    return sum
+}
+
+module.export = twoDimensionalSum
+
