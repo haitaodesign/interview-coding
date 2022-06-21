13dca29eadc5f21aa0b3918b484da1e23ff7738c
Haitao Lee <lihaitaodesign@gmail.com>
2022-06-21 09:39:36 +0800

    feat: 新增 demo

--- code-js/index.js
+++ code-js/index.js
@@ -1,7 +1,9 @@
-// import twoDimensionalSum from './twoDimensionalSum.js'
+import twoDimensionalSum from './twoDimensionalSum.js'
 import { curry3 } from './curry.js'
 
 twoDimensionalSum([[1,2,3], [2,3,4]])
 
 const sum = curry3(1,2,3)
+const sum2 = curry3(1)(2)(3)
 console.log('sum', sum)
+console.log('sum2', sum2)
